6d7f14dc49c841b75cd788730513925976a9a1bd
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-01-26 23:32:55 +0900

    add id & little modify

--- app/src/main/java/com/example/calculatortest/MainActivity.kt
+++ app/src/main/java/com/example/calculatortest/MainActivity.kt
@@ -11,7 +11,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun buttonClicked(v : View){
-
+        when(v.id){
+            R.id.button0 ->
+            R.id.button1 ->
+            R.id.button2 ->
+            R.id.button3 ->
+            R.id.button4 ->
+            R.id.button5 ->
+            R.id.button6 ->
+            R.id.button7 ->
+            R.id.button8 ->
+            R.id.button9 ->
+        }
     }
     fun resultButtonClicked(v : View){
 
