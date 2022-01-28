db65f418e7d27a8eef6fd9d6e73d72c9ec3bd6ce
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-01-28 19:57:52 +0900

    plus system

--- app/src/main/java/com/example/calculatortest/MainActivity.kt
+++ app/src/main/java/com/example/calculatortest/MainActivity.kt
@@ -3,8 +3,18 @@ package com.example.calculatortest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val expressionTextView: TextView by lazy {
+        findViewById<TextView>(R.id.expressionTextView)
+    }
+
+    private val resultTextView: TextView by lazy {
+        findViewById<TextView>(R.id.resultTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,17 +22,30 @@ class MainActivity : AppCompatActivity() {
 
     fun buttonClicked(v : View){
         when(v.id){
-            R.id.button0 ->
-            R.id.button1 ->
-            R.id.button2 ->
-            R.id.button3 ->
-            R.id.button4 ->
-            R.id.button5 ->
-            R.id.button6 ->
-            R.id.button7 ->
-            R.id.button8 ->
-            R.id.button9 ->
+            R.id.button0 -> numberButtonClicked("0")
+            R.id.button1 -> numberButtonClicked("1")
+            R.id.button2 -> numberButtonClicked("2")
+            R.id.button3 ->numberButtonClicked("3")
+            R.id.button4 ->numberButtonClicked("4")
+            R.id.button5 ->numberButtonClicked("5")
+            R.id.button6 ->numberButtonClicked("6")
+            R.id.button7 ->numberButtonClicked("7")
+            R.id.button8 ->numberButtonClicked("8")
+            R.id.button9 ->numberButtonClicked("9")
+            R.id.buttonPlus ->operatorButtonClicked("+")
+            R.id.buttonMinus ->operatorButtonClicked("-")
+            R.id.buttonDivider ->operatorButtonClicked("/")
+            R.id.buttonModulo ->operatorButtonClicked("%")
+            R.id.buttonMulti ->operatorButtonClicked("X")
         }
+    }
+
+    private fun numberButtonClicked(number: String){
+        expressionTextView.text
+    }
+    private fun operatorButtonClicked(operator : String)
+    {
+
     }
     fun resultButtonClicked(v : View){
 
