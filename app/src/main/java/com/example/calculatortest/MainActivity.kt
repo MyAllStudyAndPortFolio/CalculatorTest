e72ff27c2954013e8432f53dcca0050d4e4d7df1
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-01-31 19:51:21 +0900

    system fix & add

--- app/src/main/java/com/example/calculatortest/MainActivity.kt
+++ app/src/main/java/com/example/calculatortest/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.calculatortest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    private var isOperator = false
+    private var hasOperator = false
     fun buttonClicked(v : View){
         when(v.id){
             R.id.button0 -> numberButtonClicked("0")
@@ -41,11 +48,60 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun numberButtonClicked(number: String){
-        expressionTextView.text
+
+
+
+        val expressionText = expressionTextView.text.split(" ")
+
+        if (expressionText.isNotEmpty() && expressionText.last().length >= 15){
+            Toast.makeText(this,"15자리까지 쓰실 수 있습니다. ", Toast.LENGTH_SHORT).show()
+            return
+        }   else if ( expressionText.last().isEmpty() && number == "0"){
+            Toast.makeText(this,"0 앞에 큰 숫자는 오실수 없습니다 ", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if(isOperator == true){
+            expressionTextView.append(" ")
+        }
+
+        isOperator = false
+
+
+        expressionTextView.append(number)
     }
     private fun operatorButtonClicked(operator : String)
     {
+        if (expressionTextView.text.isEmpty()){
+            return
+        }
 
+        when {
+            isOperator->{
+                val text = expressionTextView.text.toString()
+                expressionTextView.text = text.dropLast(1) +operator
+            }
+            hasOperator ->{
+                Toast.makeText(this,"연산자는 한번만 사용할수 있습니다",Toast.LENGTH_SHORT).show()
+                return
+            }
+            else ->{
+                expressionTextView.append(" $operator")
+            }
+        }
+
+        val ssb = SpannableStringBuilder(expressionTextView.text)
+        ssb.setSpan(
+            // getcolor depcreated 되어서 contextcompat 하고 this 붙여야 한다
+            ForegroundColorSpan(ContextCompat.getColor(this,R.color.green)),
+            expressionTextView.text.length-1,
+            expressionTextView.text.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        expressionTextView.text = ssb
+
+        isOperator = true
+        hasOperator = true
     }
     fun resultButtonClicked(v : View){
 
