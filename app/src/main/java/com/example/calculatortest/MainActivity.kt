77442e4e453f5db370c421b4b30e4f9f8d49aa4e
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-02-02 03:25:03 +0900

    add system

--- app/src/main/java/com/example/calculatortest/MainActivity.kt
+++ app/src/main/java/com/example/calculatortest/MainActivity.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,36 +21,43 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.resultTextView)
     }
 
+
+
+    private var isOperator = false
+    private var hasOperator = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    private var isOperator = false
-    private var hasOperator = false
-    fun buttonClicked(v : View){
-        when(v.id){
+    fun buttonClicked(v: View) {
+        when (v.id) {
             R.id.button0 -> numberButtonClicked("0")
             R.id.button1 -> numberButtonClicked("1")
             R.id.button2 -> numberButtonClicked("2")
-            R.id.button3 ->numberButtonClicked("3")
-            R.id.button4 ->numberButtonClicked("4")
-            R.id.button5 ->numberButtonClicked("5")
-            R.id.button6 ->numberButtonClicked("6")
-            R.id.button7 ->numberButtonClicked("7")
-            R.id.button8 ->numberButtonClicked("8")
-            R.id.button9 ->numberButtonClicked("9")
-            R.id.buttonPlus ->operatorButtonClicked("+")
-            R.id.buttonMinus ->operatorButtonClicked("-")
-            R.id.buttonDivider ->operatorButtonClicked("/")
-            R.id.buttonModulo ->operatorButtonClicked("%")
-            R.id.buttonMulti ->operatorButtonClicked("X")
+            R.id.button3 -> numberButtonClicked("3")
+            R.id.button4 -> numberButtonClicked("4")
+            R.id.button5 -> numberButtonClicked("5")
+            R.id.button6 -> numberButtonClicked("6")
+            R.id.button7 -> numberButtonClicked("7")
+            R.id.button8 -> numberButtonClicked("8")
+            R.id.button9 -> numberButtonClicked("9")
+            R.id.buttonPlus -> operatorButtonClicked("+")
+            R.id.buttonMinus -> operatorButtonClicked("-")
+            R.id.buttonMulti -> operatorButtonClicked("*")
+            R.id.buttonDivider -> operatorButtonClicked("/")
+            R.id.buttonModulo -> operatorButtonClicked("%")
         }
     }
 
     private fun numberButtonClicked(number: String){
 
+        if(isOperator){
+            expressionTextView.append(" ")
+        }
 
+        isOperator = false
 
         val expressionText = expressionTextView.text.split(" ")
 
@@ -61,14 +69,9 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        if(isOperator == true){
-            expressionTextView.append(" ")
-        }
-
-        isOperator = false
-
-
         expressionTextView.append(number)
+        resultTextView.text = calculateExpression()
+
     }
     private fun operatorButtonClicked(operator : String)
     {
@@ -92,24 +95,67 @@ class MainActivity : AppCompatActivity() {
 
         val ssb = SpannableStringBuilder(expressionTextView.text)
         ssb.setSpan(
+
             // getcolor depcreated 되어서 contextcompat 하고 this 붙여야 한다
-            ForegroundColorSpan(ContextCompat.getColor(this,R.color.green)),
+            ForegroundColorSpan(ContextCompat.getColor(this, R.color.green)),
             expressionTextView.text.length-1,
             expressionTextView.text.length,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
 
         expressionTextView.text = ssb
 
         isOperator = true
         hasOperator = true
+
+
     }
+
     fun resultButtonClicked(v : View){
 
     }
+
+    private fun calculateExpression(): String {
+
+        val expressionTexts = expressionTextView.text.split(" ")
+
+        if (hasOperator.not() || expressionTexts.size != 3){
+            return ""
+        }
+
+        else if (expressionTexts[0].isNumber().not() || expressionTexts[2].isNumber().not()){
+            return ""
+        }
+
+        val exp1 = expressionTexts[0].toBigInteger()
+        val exp2 = expressionTexts[2].toBigInteger()
+        val op = expressionTexts[1]
+
+        return when(op) {
+            "+" -> (exp1 + exp2).toString()
+            "-" -> (exp1 - exp2).toString()
+            "x" -> (exp1 * exp2).toString()
+            "/" -> (exp1 / exp2).toString()
+            "%" -> (exp1 % exp2).toString()
+            else -> ""
+        }
+    }
+
     fun historyButtonClicked(v : View){
 
     }
+
     fun clearButtonClicked(v : View){
 
     }
+
+    fun String.isNumber() : Boolean {
+        return try {
+            this.toBigInteger()
+            true
+        }catch (e : NumberFormatException){
+            false
+        }
+    }
+
 }
